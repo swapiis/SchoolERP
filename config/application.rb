6efd153715669b91a6b3fab6be665ab2565bb615
Swapnil <swapiis>
2016-06-17 21:49:37 +0530

    Heroku
    Removed PGsql from  gemfile

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,6 @@ module Educulture
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
     config.active_record.raise_in_transactional_callbacks = true
+    config.secret_key_base = '6342d15d9735f56d2eef499af672f3dfe76b5eedcab930dd0fbebe46a78081525e30313c7dca07d5933033e4b03ed5fec34718f57f3fcf7e3863838095643cb1'	# this key might change for another m/c; can't hardcode in production
   end
 end
